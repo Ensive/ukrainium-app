90b511ec5e8eac41c047f066c2e9f17d8f350075
olga <olgaa.yatsuk@gmail.com>
2018-12-07 13:12:07 +0200

    contacts us; fixes;

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+(function () {
+  $('.js-slider').slick({
+    arrow: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+    prevArrow: '<a href="#" class="arrow arrow--prev"></a>',
+    nextArrow: '<a href="#" class="arrow arrow--next"></a>',
+  });
+})();
