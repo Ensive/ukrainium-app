6494ef7ae85d391d154baed69df352245cafb261
olga <olgaa.yatsuk@gmail.com>
2018-12-07 23:47:08 +0200

    pop-up with form; some content;

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,16 +9,38 @@
   });
 
   const $root = $('body,html');
-  const $order = $('.js-link');
+  const $anchor = $('.js-link');
 
 
-  $order.on('click', scrollToBlock);
+  $anchor.on('click', scrollToBlock);
 
   function scrollToBlock(e) {
     e.preventDefault();
     const id = $(this).attr('href');
-    const top = $(id).offset().top;
+    const top = $(id).offset().top - 40;
     $root.animate({scrollTop: top}, 800);
+  }
+
+  const $popUp = $('.js-pop-up');
+  const $popUpTrigger = $('.js-pop-up-button');
+  const $closePopup = $('.js-close');
+
+  $popUpTrigger.on('click', showPopUp);
+  $closePopup.on('click', hidePopUp);
+  $(document).on('mousedown', hidePopUp);
+
+  function showPopUp() {
+    $popUp.fadeIn('is-shown');
+    $('body').addClass('static');
+  }
+
+  function hidePopUp(e) {
+    $popUp.fadeOut('is-shown');
+    $('body').removeClass('static');
+
+    if ($popUp.has(e.target).length === 0){
+      $popUp.removeClass('is-shown');
+    }
   }
 })();
 
