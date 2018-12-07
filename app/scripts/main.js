54cb3ba61612f234bd68c6492588e04cc6854010
olga <olgaa.yatsuk@gmail.com>
2018-12-07 13:36:17 +0200

    mobile optimization;

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,4 +7,18 @@
     prevArrow: '<a href="#" class="arrow arrow--prev"></a>',
     nextArrow: '<a href="#" class="arrow arrow--next"></a>',
   });
+
+  const $root = $('body,html');
+  const $order = $('.js-link');
+
+
+  $order.on('click', scrollToBlock);
+
+  function scrollToBlock(e) {
+    e.preventDefault();
+    const id = $(this).attr('href');
+    const top = $(id).offset().top;
+    $root.animate({scrollTop: top}, 800);
+  }
 })();
+
